3935f173a2810b66f70ba2f122a0999450c55c0f
Chrisphine10 <ondiekchrisphine@gmail.com>
2023-11-14 21:37:15 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Button, Typography, Container, AppBar, Toolbar } from '@mui/material';
 import './App.css'; // You can create this file to add custom styles if needed
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ErrorBoundary from './ErrorBoundary';
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import { AuthProvider } from "./helpers/auth/AuthProvider";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   const defaultTheme = createTheme();
   return (
     <ErrorBoundary>
       <ThemeProvider theme={defaultTheme}>
-        <Container>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6">Material-UI React App</Typography>
-            </Toolbar>
-          </AppBar>
-          <div>
-            <Typography variant="h4">Welcome to Material-UI!</Typography>
-            <Button variant="contained" color="primary">
-              Click me
-            </Button>
-          </div>
-        </Container>
+        <Provider store={store}>
+          <AuthProvider>
+            <Router>
+              <Routes>
+                <Route path="/" element={<div>Home</div>} />
+                <Route path="/login" element={<div>Login</div>} />
+                <Route path="/register" element={<div>Register</div>} />
+                <Route path="/dashboard" element={<div>Dashboard</div>} />
+                <Route path="/dashboard/:id" element={<div>Dashboard</div>} />
+                <Route path="/profile" element={<div>Profile</div>} />
+                <Route path="/profile/:id" element={<div>Profile</div>} />
+                <Route path="/settings" element={<div>Settings</div>} />
+                <Route path="/settings/:id" element={<div>Settings</div>} />
+              </Routes>
+            </Router>
+          </AuthProvider>
+        </Provider>
       </ThemeProvider>
     </ErrorBoundary>
   );
