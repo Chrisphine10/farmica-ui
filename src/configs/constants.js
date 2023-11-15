4198fff38ec484e56d1cca9884c3526d3b68b36f
Chrisphine10 <ondiekchrisphine@gmail.com>
2023-11-15 19:44:44 +0300

    all

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-    API_URL_V1: "http://localhost:8080/api/",
-    API_URL_V2: "http://localhost:8080/api/",
-    API_AUTH_URL: "http://localhost:8080/api/",
+    API_URL_V1: " https://cd3d-154-72-41-10.ngrok-free.app/api/",
+    API_URL_V2: " https://cd3d-154-72-41-10.ngrok-free.app/api/",
+    API_AUTH_URL: " https://cd3d-154-72-41-10.ngrok-free.app/api/",
 };
